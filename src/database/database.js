afdb2d79eb6a5438d04adc05d3070b3f16beac4d
rodrigo castro <rodrigocastro@MacBook-Pro-de-rodrigo.local>
2018-12-01 17:15:24 -0300

    mLab agregado

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 const user = "rodric88"
 const pass = "rodric8888crodri"
 // mongoose.connect("mongodb://localhost/dbprueba", {useNewUrlParser: true})
-mongoose.connect(`mongodb://${user}:${pass}1@ds231228.mlab.com:31228/ddbbprueba`, {useNewUrlParser: true})
+mongoose.connect(`mongodb://${user}:${pass}@ds231228.mlab.com:31228/ddbbprueba`, {useNewUrlParser: true})
 // DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead
 // Para solucionar agregue la siguiente linea...
 mongoose.set('useCreateIndex', true);
